9505e783f3cd9cadcc90c15fa4150e6ac2902d6d
Antonio Maiocchi <antonio.maiocchi12@gmail.com>
2020-01-27 16:15:48 +0100

    add private routes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -10,8 +10,17 @@ import Header from '../../components/Header';
 // });
 
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => {}}/>);
     expect(wrapper).toMatchSnapshot();
 });
+
+
+test('should call logout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
+});
+
 
 
